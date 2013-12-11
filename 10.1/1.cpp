2784a66919ef41583efc8d574dc32ee7be82bbdb
Mozillazg <opensource.mozillazg@gmail.com>
2013-12-11 20:00:35 +0800

    update

--- 10.1/1.cpp
+++ 10.1/1.cpp
@@ -1,35 +1,31 @@
 #include <iostream>
 using namespace std;
 
-int reverse(char *s)
-{
-   if(s[0]=='\0') {
-     return 0;
-   } else {
-        reverse(&s[1]);
-        if (s[0] != ' ' && s[0] != '\0') {
-            cout << s[0];
-        }
-    }
-}
-
 int main() {
     char str[501];
+    char tmp[501];
     cin.getline(str, 501);
     int x = 0;
+    string s = string(str);
+    int len = s.size() + 1;
 
-    for (int i=0; str[i]!='\0'; i++) {
-        if (str[i] == ' ' || str[i+1] == '\0') {
-            int j = i-x;
-            if (x != 0) {
-                j++;
+    int i=0, j=0, k=0;
+    while (str[i] != '\0') {
+        if (str[i] != ' ') {
+            tmp[j] = str[i];
+            j++;
+        } else {
+            for (k=j-1; k>=0; k--) {
+                cout << tmp[k];
             }
-            reverse(&string(str).substr(x, j)[0]);
-            x = i + 1;
-            if (str[i] == ' ') {
-                cout << ' ';
-            }
+            cout << " ";
+            j=0;
         }
+        i++;
+    }
+
+    for (k=j-1; k>=0; k--) {
+        cout << tmp[k];
     }
 
 
